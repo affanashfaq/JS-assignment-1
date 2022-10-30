17961611b24160737c10f7f1b8ed14628ef847cc
unknown <affanashfaq786@gmail.com>
2022-10-30 14:43:26 -0700

    Work complete

---
+++ app.js
@@ -0,0 +1,51 @@
+var a =  20 
+var b =  30 
+var c = b - ++a -b + a - --b + a
+alert(c)
+
+var a = 10;
+var b = 20;
+
+var a = ++a;
+var b = --b;
+
+console.log(a);
+console.log(b);
+
+
+var a = 10;
+var b = 20;
+
+var a = a++;
+var b = b--;
+
+console.log(a);
+console.log(b);
+
+
+var a = 10;
+var b = 20;
+var c = b - a;
+alert(c)
+
+
+var a = 10;
+var b = 20;
+var c = a - b;
+alert(c)
+
+
+var a = 20;
+var b = 10;
+var c = a + b;
+alert(c)
+
+
+var abc  = "Welcome to this page"
+alert(abc);
+
+
+var a = 20;
+var b = 10;
+var c = a - b
+alert(c);
